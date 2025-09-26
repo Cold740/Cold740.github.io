9d5f862b6cf09fc24a32de6800dde16c263b3a48
Cold740 <113934251+Cold740@users.noreply.github.com>
2025-09-26 17:32:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-const audio = document.getElementById("mySong");
-audio.muted = false;
-audio.volume = 1.0;
+ const audio = document.getElementById("mySong");
+  const button = document.getElementById("playAnthem");
+
+  button.addEventListener("click", () => {
+    audio.muted = false; 
+    audio.volume = 1.0; 
+    audio.play().catch(err => console.error("Play error:", err));
+  });
