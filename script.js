efc0d5d7288b77516f5e4aad51f1fc37ec045ca7
Cold740 <113934251+Cold740@users.noreply.github.com>
2025-10-15 19:52:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   const audio = document.getElementById("mySong");
   const anthemButton = document.getElementById("playAnthem");
-
   let isPlaying = false;
 
   anthemButton.addEventListener("click", () => {
@@ -16,5 +16,17 @@ document.addEventListener("DOMContentLoaded", () => {
       isPlaying = true;
       anthemButton.textContent = "Pause Anthem";
     }
+  });
+
+  const faqButton = document.getElementById('faq-button');
+  const faqBox = document.getElementById('faq-box');
+  const closeFaq = document.getElementById('close-faq');
+
+  faqButton.addEventListener('click', () => {
+    faqBox.style.display = 'block';
+  });
+
+  closeFaq.addEventListener('click', () => {
+    faqBox.style.display = 'none';
   });
 });
